b7727a0bfde813c1bb672ba3d2dbd433cf516ef5
Sebastian <sebastian@sgundersen.com>
2019-10-05 15:14:19 +0200

    fix error in last commit

--- source/renderer.cpp
+++ source/renderer.cpp
@@ -5,11 +5,11 @@
 #include "surface.hpp"
 
 game_renderer::game_renderer(game_state& game) : game{ game } {
-	test_texture = no::require_texture("test");
 	shader = no::require_shader("sprite");
-	animation.frames = 6;
-	animation.set_tex_coords(0.0f, { 1.0f, 1.0f / 5.0f });
 	blank_texture = no::create_texture({ 2, 2, no::pixel_format::rgba, 0xFFFFFFFF });
+	room_transform.scale = 32.0f;
+	//animation.frames = 6;
+	//animation.set_tex_coords(0.0f, { 1.0f, 1.0f / 5.0f });
 }
 
 game_renderer::~game_renderer() {
@@ -19,8 +19,10 @@ game_renderer::~game_renderer() {
 void game_renderer::update() {
 	camera.zoom = game.zoom;
 	camera.transform.scale = game.window().size().to<float>();
-	camera.transform.position = game.world.player.transform.position - camera.transform.scale / 2.0f;
-	animation.update(1.0f / 60.0f);
+	if (!game.god_mode) {
+		camera.transform.position = game.world.player.transform.position - camera.transform.scale / 2.0f;
+	}
+	//animation.update(1.0f / 60.0f);
 }
 
 void game_renderer::draw() {
@@ -42,8 +44,6 @@ void game_renderer::render_room(const game_world_room& room) {
 	}
 	auto& rendered_room{ rendered_rooms.emplace_back() };
 	rendered_room.room = &room;
-	//rendered_room.transform.position = { static_cast<float>(room.index.x * 32), static_cast<float>(room.index.y * 32) };
-	rendered_room.transform.scale = 32.0f;
 	auto& shape{ rendered_room.shape };
 	for (int x{ room.left() }; x < room.right(); x++) {
 		for (int y{ room.top() }; y < room.bottom(); y++) {
@@ -77,7 +77,7 @@ void game_renderer::hide_room(const game_world_room& room) {
 	}
 }
 
-bool game_renderer::is_rendered(const game_world_room& room) {
+bool game_renderer::is_rendered(const game_world_room& room) const {
 	for (auto& rendered_room : rendered_rooms) {
 		if (rendered_room.room == &room) {
 			return true;
@@ -86,28 +86,13 @@ bool game_renderer::is_rendered(const game_world_room& room) {
 	return false;
 }
 
-void game_renderer::draw_world(const game_world& world) {
+void game_renderer::draw_world(const game_world& world) const {
 	no::get_shader_variable("color").set(no::vector4f{ 1.0f });
 	no::bind_texture(blank_texture);
+	no::set_shader_model(room_transform);
 	for (const auto& room : rendered_rooms) {
-		no::draw_shape(room.shape, room.transform);
+		room.shape.bind();
+		room.shape.draw();
 	}
-	no::bind_texture(test_texture);
-	const no::vector2f test_size{ no::texture_size(test_texture).to<float>() };
-	const no::vector2f size{ test_size.x / 6.0f, test_size.y / 5.0f };
-	animation.draw(world.player.transform.position, size);
-}
-
-void game_renderer::draw_room(const game_world_room& room) {
-	/*no::transform2 transform;
-	transform.scale = static_cast<float>(tile_size);
-	for (int x{ room.left() }; x < room.right(); x++) {
-		transform.position.x = static_cast<float>(x * tile_size);
-		for (int y{ room.top() }; y < room.bottom(); y++) {
-			transform.position.y = static_cast<float>(y * tile_size);
-			const no::vector4f color{ 0.5f, 0.5f, room.tile_at(x - room.index.x, y - room.index.y) == tile_type::floor ? 1.0f : 0.0f, 1.0f };
-			no::get_shader_variable("color").set(color);
-			no::draw_shape(rectangle, transform);
-		}
-	}*/
+	//animation.draw(world.player.transform.position, { texture_size.x / 6.0f, texture_size.y / 5.0f });
 }
